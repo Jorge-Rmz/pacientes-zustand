bc2e051c05fc023ed4995223f1c6e93b26a788b9
Jorge-Rmz <jorgre5675420j@gmail.com>
2025-02-02 13:44:32 -0600

    validaciones de formulario

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
+import PatientForm from "./componets/PatientForm"
+import PatientsList from "./componets/PatientsList"
 
 function App() {
   return (
     <>
 
       <div className="container mx-auto mt-20">
+        <h1 className="font-black text-5xl text-center md:w-2/3 md:mx-auto">
+          Seguimiento de Pacientes {''}
+          <span className="text-indigo-700">
+            Veterinaria
+          </span>
+        </h1>
+        <div className="mt-12 md:flex">
+          <PatientForm/>
+          <PatientsList/>
+        </div>
+
 
       </div>
     </>
---
+++ src/componets/PatientsList.tsx
@@ -0,0 +1,9 @@
+
+
+export default function PatientsList() {
+  return (
+    <div>
+      PatientsList
+    </div>
+  )
+}
